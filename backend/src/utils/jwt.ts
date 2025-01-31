54bb5452eeb5b8b8617e4bd82bfa11170c18435a
squach28 <squach3443@gmail.com>
2025-01-31 10:41:36 -0800

    change access token to expire in 15 min

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const generateAccessToken = (id: string) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "15m" });
 };
 
 export const verifyAccessToken = (token: string) => {
