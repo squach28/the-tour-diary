b26eeec222fe72c99ac923532112cb40f6e03de9
squach28 <squach3443@gmail.com>
2025-01-31 14:26:30 -0800

    create update user by id endpoint

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -35,12 +35,53 @@ export const getUserById = async (
   }
 };
 
-export const updateUserById = (req: express.Request, res: express.Response) => {
+export const updateUserById = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  const client = await db.connect();
   try {
+    const { firstName, lastName, email } = req.body;
+    const { id } = req.params;
+    if (
+      firstName === undefined ||
+      lastName === undefined ||
+      email === undefined
+    ) {
+      res.status(400).json({ message: "Field is missing" });
+      return;
+    }
+
+    if (id === undefined) {
+      res.status(400).json({ message: "ID is missing" });
+    }
+
+    if (!validator.isUUID(id)) {
+      res.status(400).json({ message: "ID is not a valid UUID" });
+      return;
+    }
+
+    const result = await client.query(userQueries.updateUserById, [
+      firstName,
+      lastName,
+      email,
+      id,
+    ]);
+
+    if (result.rowCount === 0) {
+      res.status(404).json({ message: "User with ID doesn't exist" });
+      return;
+    }
+
+    res.status(201).json(result.rows[0]);
+    return;
   } catch (e) {
     console.log(e);
+    await client.query("ROLLBACK");
     res.status(500).json({ message: "Something went wrong" });
     return;
+  } finally {
+    client.release();
   }
 };
 
--- backend/src/db/queries/users.ts
+++ backend/src/db/queries/users.ts
@@ -3,6 +3,6 @@ export const queries = {
     "INSERT INTO users (id, first_name, last_name, email) VALUES ($1, $2, $3, $4)",
   getUserById: "SELECT * FROM users WHERE id = $1",
   updateUserById:
-    "UPDATE users SET first_name = $1, last_name = $2, email = $3 WHERE id = $4 SET RETURNING *",
+    "UPDATE users SET first_name = $1, last_name = $2, email = $3 WHERE id = $4 RETURNING *",
   deleteUserById: "DELETE FROM users WHERE id = $1",
 };
