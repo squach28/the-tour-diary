c0727ae7899c31c4a0c865b116f231eaa5202fd0
squach28 <squach3443@gmail.com>
2025-01-31 14:17:38 -0800

    create get user by id endpoint

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -1,7 +1,33 @@
 import express from "express";
+import { db } from "../db/db";
+import { queries as userQueries } from "../db/queries/users";
+import validator from "validator";
 
-export const getUserById = (req: express.Request, res: express.Response) => {
+export const getUserById = async (
+  req: express.Request,
+  res: express.Response
+) => {
   try {
+    const { id } = req.params;
+    if (id === undefined) {
+      res.status(400).json({ message: "ID is missing" });
+      return;
+    }
+
+    if (!validator.isUUID(id)) {
+      res.status(400).json({ message: "ID is not a valid UUID" });
+      return;
+    }
+
+    const result = await db.query(userQueries.getUserById, [id]);
+
+    if (result.rows.length === 0) {
+      res.status(404).json({ message: "User with id doesn't exist" });
+      return;
+    }
+
+    res.status(200).json(result.rows[0]);
+    return;
   } catch (e) {
     console.log(e);
     res.status(500).json({ message: "Something went wrong" });
