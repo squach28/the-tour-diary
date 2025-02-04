53042d139bccdbcf3992f7fbee43228f6211fbfd
squach28 <squach3443@gmail.com>
2025-02-04 15:14:45 -0800

    add route for /auth/login and /auth/signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import Layout from "./layouts/Layout";
+import AuthLayout from "./layouts/auth/AuthLayout";
+import LoginForm from "./components/auth/LoginForm";
+import SignupForm from "./components/auth/SignupForm";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index={true} element={<Home />}></Route>
+      </Route>
+      <Route path="/auth" element={<AuthLayout />}>
+        <Route path="login" element={<LoginForm />}></Route>
+        <Route path="signup" element={<SignupForm />}></Route>
       </Route>
     </Routes>
   );
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <ul className="flex p-4 gap-2 bg-green-200">
+        <li>the tour diary</li>
+        <li className="ml-auto">
+          <Link to="/auth/login">Log in</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
