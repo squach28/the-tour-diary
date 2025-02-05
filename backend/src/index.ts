46dbb5f1743b5e2565ddb28d7a4bbc6fcfbca0b8
squach28 <squach3443@gmail.com>
2025-02-05 10:55:40 -0800

    add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,12 +4,18 @@ import cookieParser from "cookie-parser";
 import { authRouter } from "./routes/auth.route";
 import { usersRouter } from "./routes/users.route";
 import { concertsRouter } from "./routes/concerts.route";
+import cors from "cors";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
