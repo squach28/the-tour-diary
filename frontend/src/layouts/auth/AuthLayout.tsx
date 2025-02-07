c97eb9600f594874a8c896e800d7772dd967f6b5
squach28 <squach3443@gmail.com>
2025-02-07 01:06:57 -0800

    change link that leads to home page into inline-block

--- frontend/src/layouts/auth/AuthLayout.tsx
+++ frontend/src/layouts/auth/AuthLayout.tsx
@@ -7,7 +7,7 @@ const AuthLayout = () => {
     <Navigate to="/dashboard" />
   ) : (
     <div className="min-h-screen">
-      <Link className="block font-bold text-lg p-4" to="/">
+      <Link className="inline-block font-bold text-lg p-4" to="/">
         the tour diary
       </Link>
       <main className="pt-6 pb-6 px-12">
