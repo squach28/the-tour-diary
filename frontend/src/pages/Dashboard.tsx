ddfaffe32ee52735791b84dc18bb7bf1818f8a39
squach28 <squach3443@gmail.com>
2025-02-10 19:30:33 -0800

    navigate use to search if user submits non-empty search query

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,13 +1,27 @@
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
 const Search = () => {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (query !== "") {
+      navigate(`/search?query=${query}`);
+    }
+  };
+
   return (
-    <form className="flex relative p-3">
+    <form className="flex relative p-3" onSubmit={onSubmit}>
       <input
         className="w-full border border-gray-400 p-2 rounded-md rounded-r-none focus:outline-none focus:border-black"
         type="text"
-        placeholder="Search for an artist or event"
+        onChange={(e) => setQuery(e.target.value)}
+        value={query}
+        placeholder="Search for an artist"
       />
       <button className="bg-green-400 w-16 border border-l-red-200 rounded-md rounded-l-none">
         <FontAwesomeIcon icon={faSearch} />
