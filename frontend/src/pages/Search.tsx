f15ae54bd51e78a8a155045b45c043777a82c5d9
squach28 <squach3443@gmail.com>
2025-02-17 13:02:13 -0800

    show query for when user searches for artist

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -39,7 +39,9 @@ const Search = () => {
   return (
     <div>
       <div className="p-4">
-        <h1 className="text-2xl font-bold text-center">Results for: {query}</h1>
+        <h1 className="text-2xl font-bold text-center mb-4 border-0">
+          Results for: {query}
+        </h1>
         {loading ? <p>Loading...</p> : null}
         <ul className="flex flex-col items-center pt-4 gap-10">
           {searchResult
