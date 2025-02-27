223c12f5514891edcd59c5b6a441884cf9cc20e2
squach28 <squach3443@gmail.com>
2025-02-27 13:46:04 -0800

    changed table from user_artists to user_favorite_artists

--- backend/src/db/queries/artists.ts
+++ backend/src/db/queries/artists.ts
@@ -2,9 +2,11 @@ export const queries = {
   getArtistBySpotifyId: "SELECT * FROM artists WHERE spotify_id = $1",
   insertArtist: "INSERT INTO artists (spotify_id, mbid) VALUES ($1, $2)",
   insertArtistToUserFavorites:
-    "INSERT INTO user_artists (user_id, artist_id) VALUES ($1, $2) RETURNING id",
+    "INSERT INTO user_favorite_artists (user_id, artist_id) VALUES ($1, $2) RETURNING id",
   removeArtistFromUserFavorites:
-    "DELETE FROM user_artists WHERE user_id = $1 and artist_id = $2",
+    "DELETE FROM user_favorite_artists WHERE user_id = $1 and artist_id = $2",
   getFavoriteArtistsByUserId:
-    "SELECT user_id, artist_id FROM user_artists WHERE user_id = $1",
+    "SELECT user_id, artist_id FROM user_favorite_artists WHERE user_id = $1",
+  isArtistInUserFavorites:
+    "SELECT EXISTS (SELECT 1 FROM user_favorite_artists WHERE user_id = $1 AND artist_id = $2) AS is_favorite",
 };
